baaf5b9d8e4601ce2e48e6b032a17011527f6785
Terje Sandstrom <terje@hermit.no>
2025-08-06 18:43:37 +0200

    Updated with nunit 4.4 , and changes to the PR

--- GetChanges/GitHubApi.cs
+++ GetChanges/GitHubApi.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 
@@ -120,77 +121,30 @@ namespace Alteridem.GetChanges
         internal async Task<int> FindPullRequestNumber(int issueNumber)
         {
             var issueUrl = $"https://github.com/{_organization}/{_repository}/issues/{issueNumber}";
-            try
+
+            // Step 1: Retrieve the issue's HTML content
+            var htmlResponse = await httpClient.GetStringAsync(issueUrl);
+
+            // Step 2: Parse the HTML to find the pull request link
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(htmlResponse);
+
+            var link = htmlDoc.DocumentNode
+                .SelectSingleNode("//a[contains(@href,'/pull/')]");
+
+            if (link != null &&
+                Regex.Match(link.GetAttributeValue("href", ""), @"/pull/(\d+)")
+                    .Groups[1].Success)
             {
-                // Step 1: Retrieve the issue's HTML content
-                var htmlResponse = await httpClient.GetStringAsync(issueUrl);
-
-                // Step 2: Parse the HTML to find the pull request link
-                var htmlDoc = new HtmlDocument();
-                htmlDoc.LoadHtml(htmlResponse);
-
-                // Find the div with the class "css-truncate my-1" and extract the <a> href
-                var my1Nodes = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class, 'my-1')]");
-                var mt2Nodes = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class, 'mt-2')]");
-                var nodes = MergeHtmlNodeCollections(my1Nodes, mt2Nodes);
-                bool prFound = false;
-                if (my1Nodes != null)
-                {
-                    foreach (var node in nodes)
-                    {
-                        // Check if this node contains "pull" in its HTML
-                        if (node.InnerHtml.Contains("/pull/"))
-                        {
-                            prFound = true;
-                            // Find the first <a> tag within this specific div
-                            var firstAnchor = node.SelectSingleNode(".//a[contains(@href, '/pull/')]");
-                            if (firstAnchor != null)
-                            {
-                                var relativePrUrl = firstAnchor.GetAttributeValue("href", null);
-                                if (!string.IsNullOrEmpty(relativePrUrl))
-                                {
-                                    // Build the full URL
-                                    var prNumber = relativePrUrl.Split('/').Last();
-
-                                    return Convert.ToInt32(prNumber);
-                                }
-
-                                return -1; // $"{issueNumber}, No linked pull request found");
-                            }
-
-                            Console.WriteLine($"{issueNumber}, No <a> tag with /pull/ found in div");
-                        }
-                    }
-
-                    if (!prFound)
-                    {
-                        Console.WriteLine($"{issueNumber}, No pull request found");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"{issueNumber}, No 'my-1' divs found");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Issue {issueNumber}, Exception: {ex.Message}");
+                // Extract the pull request number from the link
+                var prNumber = Regex.Match(link.GetAttributeValue("href", ""), @"/pull/(\d+)")
+                    .Groups[1].Value;
+                return Convert.ToInt32(prNumber);
             }
 
-            return -3;
+            Console.WriteLine($"{issueNumber}, No pull request found");
+            return -1;
         }
-
-        public List<HtmlNode> MergeHtmlNodeCollections(HtmlNodeCollection collection1, HtmlNodeCollection collection2)
-        {
-            var mergedList = new List<HtmlNode>();
-
-            if (collection1 != null)
-                mergedList.AddRange(collection1);
-
-            if (collection2 != null)
-                mergedList.AddRange(collection2);
-
-            return mergedList;
-        }
+       
     }
 }
